3fc65f72ac5521225aed395a717a3f77fca0fea4
Venkatesh <Venkateshkillamsetty30@gmail.com>
2026-08-16 16:34:25 +0530

    skip 1 test

--- tests/CartCheckout.spec.ts
+++ tests/CartCheckout.spec.ts
@@ -3,7 +3,7 @@ import {Cartpage} from '../PageObjects/Cartpage';
 import {PLPpage} from '../PageObjects/PLPpage';
 import { CheckoutOverview } from '../PageObjects/CheckoutOverviewpage';
 
-test('Checkout and Complete Order', async ({ loggedInPage,CheckoutPage,page }) => {
+test.skip('Checkout and Complete Order', async ({ loggedInPage,CheckoutPage,page }) => {
 
   const cartpage = new Cartpage(page);
   const checkoutOverview = new CheckoutOverview(page);
